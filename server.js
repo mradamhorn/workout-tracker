fd84fac2960d4386563337323daee6651658c2c5
sloth740 <71675077+sloth740@users.noreply.github.com>
2021-04-23 15:26:14 -0700

    deploying

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(logger("dev"));
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
